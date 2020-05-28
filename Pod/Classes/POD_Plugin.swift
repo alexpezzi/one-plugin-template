adf491b6b7b0784daf12e0075263d1ef00e5dcbc
alexpezzi <alexpezzi@gmail.com>
2020-05-28 18:26:08 -0400

    Fixed formatting of POD_Plugin.swift

--- Pod/Classes/POD_Plugin.swift
+++ Pod/Classes/POD_Plugin.swift
@@ -1,10 +1,9 @@
-
 import Foundation
 import OneCore
 
 @objc(${POD_PLUGIN_NAME})
 final class ${POD_PLUGIN_NAME}: NSObject, Pluggable {
-    var identifier: String {
-        "${POD_PLUGIN_IDENTIFIER}"
-    }
+	var identifier: String {
+		"${POD_PLUGIN_IDENTIFIER}"
+	}
 }
